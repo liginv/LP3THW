bb076d473161d34bce46dd08908dd09de0cb0d8b
Ligin Vellakkad <ligindd@gmail.com>
2017-09-04 22:48:02 +0530

    Completed exercise 3.

---
+++ ex3.py
@@ -0,0 +1,24 @@
+# This print for the number of chickens.
+print("I will now count my chickens:")
+# This find the total number of Hens & Roosters
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+# This prints for the number of eggs.
+print("Now I will count the eggs:")
+# This doing the arithmatics.
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# This prints the statement to check for truth or false.
+print("Is it true 3 + 2 < 5 - 7?")
+# This does the calculation of the above problem.
+print(3 + 2 < 5 - 7)
+# This check the statements correctness.
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 -7)
+# This prints a statement of above statements conclusion.
+print("Oh, that's why it's False.")
+# This prompt for more.
+print("How about some more.")
+# More comparison problems.
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
