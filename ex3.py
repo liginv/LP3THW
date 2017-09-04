dfdd55c2688a2424236e9e9f4a073877291ddf0c
Ligin Vellakkad <ligindd@gmail.com>
2017-09-04 22:57:05 +0530

    Completed the study drill.

--- ex3.py
+++ ex3.py
@@ -1,24 +1,24 @@
 # This print for the number of chickens.
 print("I will now count my chickens:")
 # This find the total number of Hens & Roosters
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
 # This prints for the number of eggs.
 print("Now I will count the eggs:")
 # This doing the arithmatics.
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 # This prints the statement to check for truth or false.
 print("Is it true 3 + 2 < 5 - 7?")
 # This does the calculation of the above problem.
-print(3 + 2 < 5 - 7)
+print(3.0 + 2.0 < 5.0 - 7.0)
 # This check the statements correctness.
-print("What is 3 + 2?", 3 + 2)
-print("What is 5 - 7?", 5 -7)
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 -7.0)
 # This prints a statement of above statements conclusion.
 print("Oh, that's why it's False.")
 # This prompt for more.
 print("How about some more.")
 # More comparison problems.
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
---
+++ ex3_calc.py
@@ -0,0 +1,2 @@
+print("Length of my Name - Ligin Vellakkad:", 5 + 1 + 9)
+print("Sum of DOB:", 17 + 7 + 1991)
