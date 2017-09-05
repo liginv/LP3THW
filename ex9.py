81ed132f16c35971231521c5c2b81cb5abe96efc
Ligin Vellakkad <ligindd@gmail.com>
2017-09-05 14:31:30 +0530

    Complete study drills.

--- ex9.py
+++ ex9.py
@@ -1,11 +1,11 @@
 # Here's some new strange stuff, remember type it exactly.
-
+# Assign variables. \n is a escape charecter for new-line.
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
-
+# Print those variables
 print("Here are the days: ", days)
 print("Here are the months: ", months)
-
+# Print sentence in differnet lines as written with triple-quotes.
 print("""
 There's something going on here.
 With the three double-quotes.
