d733026bd643a4605fd7bf2c88001400363c5c9c
Ligin Vellakkad <ligindd@gmail.com>
2017-09-06 01:53:08 +0530

    Complete study drills.

--- ex13.py
+++ ex13.py
@@ -6,3 +6,6 @@ print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+
+name = input("What is you name? ")
+print(f"Hi,{name}!")
