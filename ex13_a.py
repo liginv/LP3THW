197dbdd73a9c9c0143c39379f8e0de6bc25481d8
Ligin Vellakkad <ligindd@gmail.com>
2017-09-06 01:48:00 +0530

    Complete study drills.

---
+++ ex13_a.py
@@ -0,0 +1,6 @@
+from sys import argv
+
+file_name, name, place = argv
+print("This file name is:", file_name)
+print("My name is", name)
+print("My palce is from", place)
---
+++ ex13_b.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+file_name, DOB, height, weight, hair_color, eye_color =argv
+print("This file name is", file_name)
+print("My Date of Birth is", DOB)
+print("My height is", height)
+print("My weight is", weight)
+print("My hair color is", hair_color)
+print("My eye color is", eye_color)
