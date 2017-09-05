ae04401c9f2844a69960f4694aa2e9b509f92da8
Ligin Vellakkad <ligindd@gmail.com>
2017-09-05 12:33:01 +0530

    Complete study drills.

--- ex8.py
+++ ex8.py
@@ -1,5 +1,6 @@
+# Create a variable that have 4 placeholders
 formatter = "{} {} {} {}"
-
+# Printing the result with format method of print to the variable created above
 print(formatter.format(1, 2, 3, 4))
 print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
