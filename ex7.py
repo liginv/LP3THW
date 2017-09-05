f322938479cc088246f49175094b3acf6852fd27
Ligin Vellakkad <ligindd@gmail.com>
2017-09-05 11:20:19 +0530

    Complete study drills.

--- ex7.py
+++ ex7.py
@@ -1,8 +1,10 @@
+# Print strings
 print("Mary had a little lamb.")
+# Print string with format method of print
 print("Its fleece was white as {}.".format('snow'))
 print("And everywhere that Mary went.")
-print("." * 10) # what'd that do?
-
+print("." * 10) # Multiply the string '.' 10, making it print 10 times
+# Assigning strings to variables
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +19,8 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end. try removing it ot see that happens
+# print the strings in the variables
+# the last end= ' ' insert space 
+# all these comes in oneline
 print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
