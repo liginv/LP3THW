a0c8f6369a3736fb639172684ccaf7dd8686e025
Ligin Vellakkad <ligindd@gmail.com>
2017-09-07 00:05:56 +0530

    Complete missed study drills.

--- ex15.py
+++ ex15.py
@@ -1,9 +1,19 @@
+# Importing the function/module argv from sys library in python
 from sys import argv
-
+# Assigning the varable script & filename to argv function
 script, filename = argv
-
+# Saving the open function result to a variable txt.
+# Open function is the file .txt file.
 txt = open(filename)
-
+# Printing the file name.
 print(f"Here's your file {filename}:")
+# Printing the content of the file.
 print(txt.read())
+# Closing the file.
 txt.close()
+
+# trying with inout alone.
+file = input("Enter the file name: ")
+txt1 = open(file)
+print(txt1.read())
+txt1.close()
