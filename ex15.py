72c652168e355dc2e4096016800665a5b7011ed2
Ligin Vellakkad <ligindd@gmail.com>
2017-09-06 16:15:42 +0530

    Complete study drills.

--- ex15.py
+++ ex15.py
@@ -6,10 +6,4 @@ txt = open(filename)
 
 print(f"Here's your file {filename}:")
 print(txt.read())
-
-print("Type the filename again:")
-file_again = input("> ")
-
-txt_again = open(file_again)
-
-print(txt_again.read())
+txt.close()
