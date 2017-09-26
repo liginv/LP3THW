c11bc8b3a8ffcbc90148d932db2bfeae7008cdfc
Ligin Vellakkad <ligindd@gmail.com>
2017-09-26 11:35:51 +0530

    Complete study drills of exercise 33.

--- exercise31-40/ex33.py
+++ exercise31-40/ex33.py
@@ -1,16 +1,17 @@
-i = 0
-numbers = []
+def while_loop(m, n):
+    i = 0
+    numbers = []
+    while i < m:
+        print(f"At the top i is {i}")
+        numbers.append(i)
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+        i = i + n
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
 
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
+    print("The numbers: ")
 
+    for num in numbers:
+        print(num)
 
-print("The numbers: ")
-
-for num in numbers:
-    print(num)
+while_loop(6, 1)
