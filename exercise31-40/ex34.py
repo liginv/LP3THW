09a0c119bdda03c2d46f33b13d82d632692c27d3
Ligin Vellakkad <ligindd@gmail.com>
2017-09-26 12:59:56 +0530

    Complete exercise 34.

---
+++ exercise31-40/ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f"The animal at 1: {animals[1]}")
+print(f"The third (3rd) animal: {animals[2]}")
+print(f"The first (1st) animal: {animals[0]}")
+print(f"The animal at 3: {animals[3]}")
+print(f"The fifth (5th) animal: {animals[4]}")
+print(f"The animal at 2: {animals[2]}")
+print(f"The sixth (6th) animal: {animals[5]}")
+print(f"The animal at 4: {animals[4]}")
